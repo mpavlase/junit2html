53143935c9b9fac237e6d30286e544b87ee5bd76
Martin Pavlasek <mpavlase@redhat.com>
2018-11-14 22:28:59 +0100

    fix typo in _init_ function
    Actually this fix will not affect the code itself, but still it deserves
    to be corrected.

--- junit2htmlreport/parser.py
+++ junit2htmlreport/parser.py
@@ -66,7 +66,7 @@ class Property(AnchorBase):
     """
     Test Properties
     """
-    def _init_(self):
+    def __init__(self):
         super(Property, self).__init__()
         self.name=None
         self.value=None
